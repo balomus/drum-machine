ffc6fc5b2d71a8156e2d3511ded36b553fb0aa20
Stephen Mercier <sjmercier@wcu.edu>
2021-12-27 13:18:11 -0500

    Click functionality working for initial sound bank.

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,16 @@
 const Button = (props) => {
+    const url = "https://s3.amazonaws.com/freecodecamp/drums/"
+
+    const handleClick = () => {
+        console.log("clicked " + props.id);
+        document.getElementById(props.id).play();
+    }
+
     return ( 
-        <div className="button">
+        <div className="button" onClick={handleClick}>
             {/* {props.id} */}
             {props.button}
+            <audio id={props.id} src={`${url}${props.id}.mp3`}></audio>
         </div>
      );
 }
