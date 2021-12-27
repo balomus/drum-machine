22bb3da9386841df14b5d8a9bdf7204176874bb7
Stephen Mercier <balom.us@gmail.com>
2021-12-26 20:49:08 -0500

    Added Button component, working on some styling as well.

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+const Button = (props) => {
+    return ( 
+        <div className="button">
+            {/* {props.id} */}
+            {props.button}
+        </div>
+     );
+}
+ 
+export default Button;
---
+++ src/Controls.js
@@ -0,0 +1,10 @@
+const Controls = () => {
+    return ( 
+        <div id="controls">
+            <div>Controls go here</div>
+            <div id="display">Display goes here</div>
+        </div>
+     );
+}
+ 
+export default Controls;
